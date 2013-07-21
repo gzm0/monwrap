b9d296c385c056cf7d532e02722d6736877317f3
Tobias Schlatter <tobias@meisch.ch>
2013-07-21 15:57:09 -0700

    Make MW work with arguments of type MW

--- src/main/scala/monwrap/MW.scala
+++ src/main/scala/monwrap/MW.scala
@@ -3,9 +3,10 @@ package monwrap
 import language.experimental.macros
 import language.dynamics
 
+import scala.annotation.tailrec
 import scala.reflect.macros.Context
 
-class MW[T,M[V]](
+class MW[T,M[_]](
   val __mon: M[T]
 ) extends AnyVal with Dynamic {
   def applyDynamic(n: String)(arg: Any*) = macro MW.appDynImpl[T,M]
@@ -13,24 +14,64 @@ class MW[T,M[V]](
 
 object MW {
 
-  def appDynImpl[T : c.WeakTypeTag, M[V]](c: Context { type PrefixType = MW[T,M] })
-  (n: c.Expr[String])(arg: c.Expr[Any]*) = {
+  def appDynImpl[T : c.WeakTypeTag, M[_]](c: Context { type PrefixType = MW[T,M] })
+                                         (n: c.Expr[String])
+                                         (arg: c.Expr[Any]*) 
+                                         (implicit monTT: c.WeakTypeTag[M[_]]) = {
   
     import c.universe._
 
-    def call(on: Tree)(f: String)(args: c.Expr[Any]*) = {
+    def callT(on: Tree)(f: String)(args: Tree*): Tree = {
       val fT = newTermName(f)
-      val arglist = args.map(_.tree).toList
-      c.Expr[Any](Apply(Select(on, fT), arglist))
+      Apply(Select(on, fT), args.toList)
+    }                                       
+    def callE(on: Tree)(f: String)(args: c.Expr[Any]*): c.Expr[Any] =
+      c.Expr[Any](callT(on)(f)(args.map(_.tree) :_*))
+
+    def anfun(tns: TermName*)(tpes: TypeTree*)(b: Tree) = {
+      import Flag._
+      val mods = Modifiers(PARAM)
+      val defs = for ((tn,tpe) <- tns zip tpes) yield ValDef(mods, tn, tpe, EmptyTree)
+      Function(defs.toList, b)
     }
+
+    def mwType(t: Type) = {
+      val sym = weakTypeOf[MW[_,M]].typeSymbol
+      val mSym = weakTypeOf[M[_]].typeSymbol
+      t match {
+        // TODO check for correct package!
+        case TypeRef(_, `sym`, List(t,TypeRef(_,`mSym`,_))) => Some(t)
+        case _ => None
+      }
+    }
+ 
+    @tailrec
+    def procArgs(
+      args_in:  List[c.Expr[Any]],
+      args_out: List[Tree],
+      aInd: Int)(wrap: Tree => Tree): (List[Tree], Tree => Tree) = args_in match {
+      case a :: as => mwType(a.tree.tpe) match {
+        case Some(tpe) =>
+          val tn = newTermName("arg" + aInd)
+          val mon = callT(Ident(newTermName("monwrap")))("unwrap")(a.tree)
+          procArgs(as, Ident(tn) :: args_out, aInd + 1){ t =>
+            callT(mon)("flatMap")(anfun(tn)(TypeTree(tpe))(wrap(t)))
+          }
+        case None =>
+          procArgs(as, a.tree :: args_out, aInd)(wrap)
+      }
+      case Nil => (args_out.reverse, wrap)
+    }
+
+    val (al, wrap) = procArgs(arg.toList, Nil, 0)(x => x)
 
     val Literal(Constant(fn: String)) = n.tree
 
-    val x = reify { v: T => call(Ident(newTermName("v")))(fn)(arg :_*).splice }
+    val x = reify { v: T => c.Expr[Any](callT(Ident(newTermName("v")))(fn)(al :_*)).splice }
 
     val mon = reify { unwrap(c.prefix.splice) }
 
-    call(mon.tree)("map")(x)
+    c.Expr[Any](wrap(callE(mon.tree)("map")(x).tree))
 
   }
 
--- src/test/scala/monwrap/dummy.scala
+++ src/test/scala/monwrap/dummy.scala
@@ -2,8 +2,9 @@ package monwrap
 
 object Test extends App {
 
-  val w = new MW(Some("asdf"))
+  val w = new MW[String,Option](Some("asdf"))
+  val i = new MW[Int,Option](Some(1))
 
-  println(w.substring(1,2))
+  println(w.substring(i))
 
 }
